224a6fef8e8060ec35d3065a3526cf6f41ac8687
ZeusCoder <zeusselalusalah@gmail.com>
2022-04-08 09:19:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const bycript = require("bcryptjs");
-
-const { user_game } = require("./models");
-const { user_game_biodata } = require("./models");
-const { user_game_history } = require("./models");
 
 const userRoutes = require("./routes/UserGame");
 const userBiodataRoutes = require("./routes/UserGameBiodata");
@@ -28,7 +23,28 @@ app.use((req, res, next) => {
 });
 
 // /api/v1/user/all
-
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Welcome to User Game API",
+    endpoint: [
+      "/api/v1/user/all",
+      "/api/v1/user/:id",
+      "/api/v1/user/create",
+      "/api/v1/user/:id/update",
+      "/api/v1/user/:id/delete",
+      "/api/v1/user/biodata/all",
+      "/api/v1/user/biodata/:id",
+      "/api/v1/user/biodata/create",
+      "/api/v1/user/biodata/:id/update",
+      "/api/v1/user/biodata/:id/delete",
+      "/api/v1/user/history/all",
+      "/api/v1/user/history/:id",
+      "/api/v1/user/history/create",
+      "/api/v1/user/history/:id/update",
+      "/api/v1/user/history/:id/delete",
+    ],
+  });
+});
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/user/biodata", userBiodataRoutes);
 app.use("/api/v1/user/history", userHistoryRoutes);
